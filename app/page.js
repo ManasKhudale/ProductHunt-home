157b238324e98db1d699cc2779bdcb0d1f7296fe
Manas Khudale <93604854+ManasKhudale@users.noreply.github.com>
2024-08-06 22:28:25 +0530

    Initial Commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,35 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import './main.css';
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className = 'nav-section-1'>
+        <nav className="inactive-nav">Home</nav> &gt; <nav className="inactive-nav"> Product</nav> &gt; <span className="active-nav">Video Dubber - Fast Video Translator</span>
+      </div>
+      <Image src="/producthuntlogo.avif" className='producthuntlogo' alt="Video Dubber" width={72} height={72} />
+      <div className="product-section">
+      <div className="main-title">VideoDubber - Fast Video Translator</div>
+      <div className = 'sub-title'>Translate videos in your own voice, globalize in a click!
+      <div className="buttons-section">
+      
+        <div className="visit-button">Visit
+        <div className="dropdown">
+        <div className='dropdown-content'>
+        <Link href='/' style={{color: 'rgba(3,3,3,255)', textDecoration: 'none'}} ><div><span className="dropdown-main">Website</span> <span className="dropdown-sub">videodubber.ai</span></div></Link>
+        <Link href='/' style={{color: 'rgba(3,3,3,255)', textDecoration: 'none'}}><div><span className="dropdown-main">Learn More</span> <span className="dropdown-sub">videodubber.ai</span></div></Link>
+        <Link href='/' style={{color: 'rgba(3,3,3,255)', textDecoration: 'none'}}><div><span className="dropdown-main">Learn More</span> <span className="dropdown-sub">videodubber.ai</span></div></Link>
+        <Link href='/' style={{color: 'rgba(3,3,3,255)', textDecoration: 'none'}} ><div><span className="dropdown-main">Learn More</span><span className="dropdown-sub">videodubber.ai</span></div></Link>
         </div>
+        </div>
+        </div>  
+        <button className="upvote-button">UPVOTE<span className="vote-count">140</span></button>
+      </div></div>
       </div>
+      
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
